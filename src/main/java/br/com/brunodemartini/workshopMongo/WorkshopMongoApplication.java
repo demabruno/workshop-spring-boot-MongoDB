f892c02fa1fda3345f862b05f47fe00a293a7041
Bruno Demartini <bruno.demartini@gmail.com>
2020-07-18 09:35:06 -0300

    Projeto Criado

---
+++ src/main/java/br/com/brunodemartini/workshopMongo/WorkshopMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunodemartini.workshopMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoApplication.class, args);
+	}
+
+}
